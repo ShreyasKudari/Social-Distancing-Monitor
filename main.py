0d3bfcfac4f85008b0ccfcb01577c9025496d5b6
A-Tsao <65750588+A-Tsao@users.noreply.github.com>
2020-08-12 02:01:40 -0500

    Basic FireBase and Flask Code
    Added basic template/boilerplate code for FireBase and Flask

---
+++ main.py
@@ -0,0 +1,46 @@
+# Core Functionalities
+# a) Real-time collection of datapoints from video feeds
+#      CV algo spits out data every so often -> update DB -> Run averaging calculation to generate index -> Push to frontend
+# b) Real-time display of data to frontend
+#      Read from DB every so often -> dump data to Google Map API
+
+# To-do:
+# Do users upload own vids? Or are we just using public CCTV?
+# Is Flask/web framework necessary?  User interaction seems to be solely interacting with map
+#   Set up pipeline btwn CV algo and DB (be mindful of FireBase read/write limits)
+#   Frontend datadump set on timer?
+
+from flask import Flask, render_template, request
+import pyrebase
+
+# Basic FireBase Connection
+config = {
+    "apiKey": "AIzaSyAf58ybK5T7_dZr10b4tum-XAu3aqu1v_Q",
+    "authDomain": "social-distancing-monito-c9b2c.firebaseapp.com",
+    "databaseURL": "https://social-distancing-monito-c9b2c.firebaseio.com",
+    "projectId": "social-distancing-monito-c9b2c",
+    "storageBucket": "social-distancing-monito-c9b2c.appspot.com",
+    "messagingSenderId": "847811129335",
+    "appId": "1:847811129335:web:505c7263ef941efeaf88f2",
+    "measurementId": "G-H2DKQT2CK8"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+db.child("names").remove()
+
+# Basic Flask Template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if request.method == 'POST':
+        pass
+    else:
+        pass
+
+    return "Hello, world!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
