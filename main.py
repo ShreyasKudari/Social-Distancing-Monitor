cb50f6d9cd706d850df7c3a2dc0ab2ed3974055e
Michelle Wen <wenmichelle07@gmail.com>
2020-08-14 01:37:40 -0500

    working website with firebase integration

--- main.py
+++ main.py
@@ -1,71 +1,71 @@
-# Core Functionalities
-# a) Real-time collection of datapoints from video feeds
-#      CV algo spits out data every so often -> update DB -> Run averaging calculation to generate index -> Push to frontend
-# b) Real-time display of data to frontend
-#      Read from DB every so often -> dump data to Google Map API
+# # Core Functionalities
+# # a) Real-time collection of datapoints from video feeds
+# #      CV algo spits out data every so often -> update DB -> Run averaging calculation to generate index -> Push to frontend
+# # b) Real-time display of data to frontend
+# #      Read from DB every so often -> dump data to Google Map API
 
-# To-do:
-# Do users upload own vids? Or are we just using public CCTV?
-# Is Flask/web framework necessary?  User interaction seems to be solely interacting with map
-#   Set up pipeline btwn CV algo and DB (be mindful of FireBase read/write limits)
-#   Frontend datadump set on timer?
+# # To-do:
+# # Do users upload own vids? Or are we just using public CCTV?
+# # Is Flask/web framework necessary?  User interaction seems to be solely interacting with map
+# #   Set up pipeline btwn CV algo and DB (be mindful of FireBase read/write limits)
+# #   Frontend datadump set on timer?
 
-from flask import Flask, render_template, request, jsonify
-import requests
-# import pyrebase
+# from flask import Flask, render_template, request, jsonify
+# import requests
+# # import pyrebase
 
-# Basic FireBase Connection
-config = {
-    "apiKey": "AIzaSyAf58ybK5T7_dZr10b4tum-XAu3aqu1v_Q",
-    "authDomain": "social-distancing-monito-c9b2c.firebaseapp.com",
-    "databaseURL": "https://social-distancing-monito-c9b2c.firebaseio.com",
-    "projectId": "social-distancing-monito-c9b2c",
-    "storageBucket": "social-distancing-monito-c9b2c.appspot.com",
-    "messagingSenderId": "847811129335",
-    "appId": "1:847811129335:web:505c7263ef941efeaf88f2",
-    "measurementId": "G-H2DKQT2CK8"
-}
+# # Basic FireBase Connection
+# config = {
+#     "apiKey": "AIzaSyAf58ybK5T7_dZr10b4tum-XAu3aqu1v_Q",
+#     "authDomain": "social-distancing-monito-c9b2c.firebaseapp.com",
+#     "databaseURL": "https://social-distancing-monito-c9b2c.firebaseio.com",
+#     "projectId": "social-distancing-monito-c9b2c",
+#     "storageBucket": "social-distancing-monito-c9b2c.appspot.com",
+#     "messagingSenderId": "847811129335",
+#     "appId": "1:847811129335:web:505c7263ef941efeaf88f2",
+#     "measurementId": "G-H2DKQT2CK8"
+# }
 
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
+# firebase = pyrebase.initialize_app(config)
+# db = firebase.database()
 
-db.child("names").remove()
+# db.child("names").remove()
 
-# Basic Flask Template
+# # Basic Flask Template
 
-# run "set FLASK_APP=main.py" and then "flask run"
+# # run "set FLASK_APP=main.py" and then "flask run"
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-example_data = {
-    'loc1': [
-        {
-            'name':'dallas',
-            'coords':'12345'
-        }
-    ]
-}
+# example_data = {
+#     'loc1': [
+#         {
+#             'name':'dallas',
+#             'coords':'12345'
+#         }
+#     ]
+# }
 
-data_json = json.dumps(example_data)
+# data_json = json.dumps(example_data)
 
-@app.route('/hello', methods=['POST', 'GET'])
-def index():
+# @app.route('/hello', methods=['POST', 'GET'])
+# def index():
 
-    # POST request
-    if request.method == 'POST':
-        print('Incoming...')
-        print(request.get_json())
-        return 'OK'
+#     # POST request
+#     if request.method == 'POST':
+#         print('Incoming...')
+#         print(request.get_json())
+#         return 'OK'
 
-    # GET request    
-    else:
-        message = {'greeting': 'Hello from Flask!'}
-        return jsonify(message)
+#     # GET request    
+#     else:
+#         message = {'greeting': 'Hello from Flask!'}
+#         return jsonify(message)
 
     
-# @app.route('/test')
-# def test_page():
-#     return render_template('index.html')
+# # @app.route('/test')
+# # def test_page():
+# #     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
--- map.js
+++ map.js
@@ -1,47 +1,63 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAf58ybK5T7_dZr10b4tum-XAu3aqu1v_Q",
+    authDomain: "social-distancing-monito-c9b2c.firebaseapp.com",
+    databaseURL: "https://social-distancing-monito-c9b2c.firebaseio.com/",
+    projectId: "social-distancing-monito-c9b2c",
+    storageBucket: "social-distancing-monito-c9b2c.appspot.com",
+    messagingSenderId: "847811129335",
+    appId: "1:847811129335:web:505c7263ef941efeaf88f2",
+    measurementId: "G-H2DKQT2CK8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+
+var ref = firebase.database().ref();
+
+var data;
+
+ref.on('child_added', function(child) {
+    console.log('child added:');
+    data = child.val();
+    addMarker(data);
+});
+ref.on('child_changed', function(child) {
+    console.log('child changed:');
+    data = child.val();
+    updateMarker(data);
+});
+
 var map;
 
-fetch('/hello')
-    .then(function (response) {
-        console.log('Success!', response);
-        //return response.json();
-    }).then(function (data) {
-        console.log('GET response as JSON: ');
-        console.log(data);
-    }).catch(function (err) {
-        console.warn('Something went wrong.', err);
-    });    
+// var markers = [
+//     // {
+//     //     coords: {lat:32.7767,lng:-96.7970}, //dallas
+//     //     index: 25,
+//     //     content: '<h1 style="color=black;">Location</h1>'
+//     // },
+//     {
+//         coords:{lat:32.7555,lng:-97.3308}, //fort worth
+//         index: 40,
+//         content: '<p style="color=black;">60% of people wear masks at this location</p>'
+//     },
+//     {
+//         coords:{lat:32.7357,lng:-97.1081}, //arlington
+//         index: 60,
+//         content: '<p style="color=black;">95% of people wear masks at this location</p>'
+//     },
+//     {
+//         coords:{lat:33.1507,lng:-96.8236}, //frisco
+//         index: 99,
+//         content: '<p style="color=black;">95% of people wear masks at this location</p>'
+//     }
+// ];
 
-
-
-//var infoObject = JSON.parse()
-
-
-var markers = [
-    {
-        coords: {lat:32.7767,lng:-96.7970}, //dallas
-        index: 25,
-        content: '<h1 style="color=black;">Location</h1>'
-    },
-    {
-        coords:{lat:32.7555,lng:-97.3308}, //fort worth
-        index: 40,
-        content: '<p style="color=black;">60% of people wear masks at this location</p>'
-    },
-    {
-        coords:{lat:32.7357,lng:-97.1081}, //arlington
-        index: 60,
-        content: '<p style="color=black;">95% of people wear masks at this location</p>'
-    },
-    {
-        coords:{lat:33.1507,lng:-96.8236}, //frisco
-        index: 99,
-        content: '<p style="color=black;">95% of people wear masks at this location</p>'
-    }
-];
+var markers = [];
+var windows = [];
 
 function initMap(){
     var options = {
-        zoom:8,
+        zoom:9,
         center:{lat:32.7767,lng:-96.7970}
     }
 
@@ -64,9 +80,9 @@ function initMap(){
 
 
 
-    for(var i = 0; i < markers.length; i++){
-        addMarker(markers[i]);
-    }
+    // for(var i = 0; i < markers.length; i++){
+    //     addMarker(markers[i]);
+    // }
 
     // var markers = locations.map(function(location, i) {
     //     return new google.maps.Marker({
@@ -86,14 +102,14 @@ function initMap(){
             };
 
             infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
+            infoWindow.setContent('<h8 style="color:black">Current Location</h8>');
             infoWindow.open(map);
             map.setCenter(pos);
+            windows.push(infoWindow);
         }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
         });
     } else {
-        // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
 
@@ -105,21 +121,67 @@ function initMap(){
         infoWindow.open(map);
     }
 
+    google.maps.event.addListener(map, "click", function(event) {
+        for (var i = 0; i < windows.length; i++ ) {
+             windows[i].close();
+        }
+    });
+
 
 }
 
 function addMarker(props){
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(props.lat, props.lng),
+        map:map,
+        icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 15,
+            fillColor: '#000000',
+            fillOpacity: 0.35,
+            strokeColor: '#000000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+        },
+    });
+
+    updateColor(props, marker);
+
+    // add info window
+    var infoWindow = new google.maps.InfoWindow({
+        content: `<h8 style="color:black">${props.avgIndex}% mask-wearers in this area</h8>`
+    });
+
+    marker.addListener('click', function(){
+        infoWindow.open(map, marker);
+    })
+
+    windows.push(infoWindow);
+
+    markers.push(marker);
+}
+
+function updateMarker(newMarker){
+    for (var i=0; i < markers.length; i++) {
+        var m = markers[i];
+        if (m.getPosition().lat() === newMarker.lat && m.getPosition().lng() === newMarker.lng) {
+            updateColor(newMarker, m);
+        }
+    }
+
+}
+
+function updateColor(newMarker, m){
+    var mag = newMarker.avgIndex;
     var cFill;
     var cStroke;
-    mag = props.index;
-    // red, orange, yellow, green
     if(mag < 25){ 
         cFill = "#FF0000"
         cStroke = "#FF0000"
     }
     else if(mag >= 25 && mag < 50){
-        cFill = "#FF6600"
-        cStroke = "#FF6600"
+        cFill = "#FFA500"
+        cStroke = "#FFA500"
     }
     else if(mag >= 50 && mag < 75){
         cFill = "#FFFF00"
@@ -130,28 +192,15 @@ function addMarker(props){
         cStroke = "#00FF00"
     }
 
-    var marker = new google.maps.Marker({
-        position:props.coords,
-        map:map,
-        icon: {
-            path: google.maps.SymbolPath.CIRCLE,
-            scale: 25,
-            fillColor: cFill,
-            fillOpacity: 0.35,
-            strokeColor: cStroke,
-            strokeOpacity: 0.8,
-            strokeWeight: 2
-        },
-    });
+    const icon = {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 15,
+        fillColor: cFill,
+        fillOpacity: 0.35,
+        strokeColor: cStroke,
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+    };
 
-    // checking for content
-    if(props.content){
-        var infoWindow = new google.maps.InfoWindow({
-            content:props.content
-        });
-
-        marker.addListener('click', function(){
-            infoWindow.open(map, marker);
-        })
-    }
+    m.setIcon(icon);
 }
